f5635e04ca2ee09f444d74c024b83160beb5580e
William Gabriel Forneck <62662711+williamforneck@users.noreply.github.com>
2024-07-04 01:56:04 -0300

    update port to render

--- api/src/server.js
+++ api/src/server.js
@@ -44,5 +44,5 @@ app.use((err, request, response, next) => {
   });
 });
 
-const PORT = 3333;
+const PORT = process.env.PORT || 3333;
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
